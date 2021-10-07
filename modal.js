1df8895abfa0982425e680f12e66ffe1f7edaa07
Vladimir <vladimirdegty@gmail.com>
2021-10-07 12:51:32 +0300

    main

--- modal.js
+++ modal.js
@@ -1,34 +1,53 @@
 const CLASS_LIST_LOGIN = {
-    MODAL: 'login-modal',
-    MODAL_ACTIVE: 'login-modal-active',
-    TRIGGER_OPEN: 'js-modal-open',
-    TRIGGER_CLOSE: 'js-modal-close'
+    MODAL_LOGIN: 'login-modal',
+    MODAL_ACTIVE_LOGIN: 'login-modal-active',
+    TRIGGER_OPEN_LOGIN: 'js-modal-open',
+    TRIGGER_CLOSE_LOGIN: 'js-modal-close'
+};
+
+const CLASS_LIST_NEWSLETTER = {
+    MODAL_NEWSLETTER: 'newsletter-modal',
+    MODAL_ACTIVE_NEWSLETTER: 'login-modal-active',
+    TRIGGER_OPEN_NEWSLETTER: 'newsletter-modal-open',
+    TRIGGER_CLOSE_NEWSLETTER: 'js-modal-close'
 };
 
 
 document.addEventListener('click', (event) => {
     //open
-    if (event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_OPEN}`)) {
+    if (event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_OPEN_LOGIN}`)) {
         console.log('open');
         event.preventDefault();
 
-        const target = event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_OPEN}`);
+        const target = event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_OPEN_LOGIN}`);
         const modalId = target.getAttribute('href').replace('#', '');
         const modal = document.getElementById(modalId);
         document.body.style.overflow = 'hidden';
 
-        modal.classList.add(CLASS_LIST_LOGIN.MODAL_ACTIVE);
+        modal.classList.add(CLASS_LIST_LOGIN.MODAL_ACTIVE_LOGIN);
+
+    }
+
+    if (event.target.closest(`.${CLASS_LIST_NEWSLETTER.TRIGGER_OPEN_NEWSLETTER}`)) {
+        console.log('open');
+        event.preventDefault();
+
+        const target = event.target.closest(`.${CLASS_LIST_NEWSLETTER.TRIGGER_OPEN_NEWSLETTER}`);
+        const modalId = target.getAttribute('href').replace('#', '');
+        const modal = document.getElementById(modalId);
+        document.body.style.overflow = 'hidden';
+        modal.classList.add(CLASS_LIST_NEWSLETTER.MODAL_ACTIVE_NEWSLETTER);
 
     }
 
     //close
-    if (event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_CLOSE}`) ||
-        event.target.classList.contains(CLASS_LIST_LOGIN.MODAL_ACTIVE)
+    if (event.target.closest(`.${CLASS_LIST_LOGIN.TRIGGER_CLOSE_LOGIN}`) ||
+        event.target.classList.contains(CLASS_LIST_LOGIN.MODAL_ACTIVE_LOGIN)
     ) {
         console.log('close');
         event.preventDefault();
-        const modal = event.target.closest(`.${CLASS_LIST_LOGIN.MODAL}`);
+        const modal = event.target.closest(`.${CLASS_LIST_LOGIN.MODAL_LOGIN}`);
         document.body.style.overflow = 'visible';
-        modal.classList.remove(CLASS_LIST_LOGIN.MODAL_ACTIVE);
+        modal.classList.remove(CLASS_LIST_LOGIN.MODAL_ACTIVE_LOGIN);
     }
 });
