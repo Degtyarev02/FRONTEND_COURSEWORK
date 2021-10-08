c68c477ffc66bc6055b6d53de21209e61bae8bf2
Vladimir <vladimirdegty@gmail.com>
2021-10-08 13:11:11 +0300

    main

--- modal.js
+++ modal.js
@@ -9,6 +9,7 @@ const CLASS_LIST_NEWSLETTER = {
     MODAL_NEWSLETTER: 'newsletter-modal',
     MODAL_ACTIVE_NEWSLETTER: 'login-modal-active',
     TRIGGER_OPEN_NEWSLETTER: 'newsletter-modal-open',
+    TRIGGER_OPEN_REGISTRATION: 'registration-modal-open',
     TRIGGER_CLOSE_NEWSLETTER: 'js-modal-close'
 };
 
@@ -33,6 +34,23 @@ document.addEventListener('click', (event) => {
         event.preventDefault();
 
         const target = event.target.closest(`.${CLASS_LIST_NEWSLETTER.TRIGGER_OPEN_NEWSLETTER}`);
+        const modalId = target.getAttribute('href').replace('#', '');
+        const modal = document.getElementById(modalId);
+        document.body.style.overflow = 'hidden';
+        modal.classList.add(CLASS_LIST_NEWSLETTER.MODAL_ACTIVE_NEWSLETTER);
+
+    }
+
+    if (event.target.closest(`.${CLASS_LIST_NEWSLETTER.TRIGGER_OPEN_REGISTRATION}`)) {
+        console.log('open');
+        event.preventDefault();
+
+        const modal1 = event.target.closest(`.${CLASS_LIST_LOGIN.MODAL_LOGIN}`);
+        document.body.style.overflow = 'visible';
+        modal1.classList.remove(CLASS_LIST_LOGIN.MODAL_ACTIVE_LOGIN);
+
+
+        const target = event.target.closest(`.${CLASS_LIST_NEWSLETTER.TRIGGER_OPEN_REGISTRATION}`);
         const modalId = target.getAttribute('href').replace('#', '');
         const modal = document.getElementById(modalId);
         document.body.style.overflow = 'hidden';
